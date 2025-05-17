e2685bb7fbeee1f038cbc22e2e1e16873aea7202
diegomao <diegomcode@gmail.com>
2025-05-17 17:59:35 -0500

    chore: commit inicial

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,21 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
+<<<<<<< HEAD
 import { LoginComponent } from './pages/login/login.component';
 import { AuthComponent } from './auth/auth.component';
+=======
+>>>>>>> 059de6d (chore: commit inicial)
 
 export const routes: Routes = [
   { path: '', redirectTo: 'register', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
+<<<<<<< HEAD
   { path: 'login', component: LoginComponent },
   { path: 'auth', component: AuthComponent },
 
 ];
 
+=======
+  // Puedes agregar el login luego
+];
+>>>>>>> 059de6d (chore: commit inicial)
