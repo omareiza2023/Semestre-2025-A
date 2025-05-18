9a25ca18296725b008215dcbc9fb107d3c56a618
diegomao <diegomcode@gmail.com>
2025-05-18 09:43:26 -0500

    fix conflicts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,21 +1,8 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
-<<<<<<< HEAD
-=======
-import { LoginComponent } from './pages/login/login.component';
-import { AuthComponent } from './auth/auth.component';
->>>>>>> feature/HU-04
 
 export const routes: Routes = [
   { path: '', redirectTo: 'register', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
-<<<<<<< HEAD
   // Puedes agregar el login luego
 ];
-=======
-  { path: 'login', component: LoginComponent },
-  { path: 'auth', component: AuthComponent },
-
-];
-
->>>>>>> feature/HU-04
