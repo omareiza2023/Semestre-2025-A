4905aca1f766d519b4e151e2b1e8f7a2183bca10
Oscar Mauricio Areiza Paramo <omareiza-2023a@corhuila.edu.co>
2025-05-17 22:37:33 -0500

    feature/HU-04
    git push origin feature/HU-05 --forcegit push origin feature/HU-05 --force#

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,22 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { LoginComponent } from './pages/login/login.component';
 import { AuthComponent } from './auth/auth.component';
 =======
 >>>>>>> 059de6d (chore: commit inicial)
+=======
+=======
+import { LoginComponent } from './pages/login/login.component';
+import { AuthComponent } from './auth/auth.component';
+>>>>>>> b213279 (feature/HU-04)
+>>>>>>> c5bbe94 (feature/HU-04)
 
 export const routes: Routes = [
   { path: '', redirectTo: 'register', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
+<<<<<<< HEAD
 <<<<<<< HEAD
   { path: 'login', component: LoginComponent },
   { path: 'auth', component: AuthComponent },
@@ -19,3 +27,14 @@ export const routes: Routes = [
   // Puedes agregar el login luego
 ];
 >>>>>>> 059de6d (chore: commit inicial)
+=======
+  // Puedes agregar el login luego
+];
+=======
+  { path: 'login', component: LoginComponent },
+  { path: 'auth', component: AuthComponent },
+
+];
+
+>>>>>>> b213279 (feature/HU-04)
+>>>>>>> c5bbe94 (feature/HU-04)
