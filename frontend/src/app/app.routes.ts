fec23ba9690892b876967f9eb82ccedbc8a825a8
Oscar Mauricio Areiza Paramo <omareiza-2023a@corhuila.edu.co>
2025-05-18 09:14:26 -0500

    delete folders

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,40 +1,19 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './pages/register/register.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { LoginComponent } from './pages/login/login.component';
-import { AuthComponent } from './auth/auth.component';
-=======
->>>>>>> 059de6d (chore: commit inicial)
-=======
-=======
-import { LoginComponent } from './pages/login/login.component';
-import { AuthComponent } from './auth/auth.component';
->>>>>>> b213279 (feature/HU-04)
->>>>>>> c5bbe94 (feature/HU-04)
+import { ViewClientComponent } from './pages/view-client/view-client.component';
+import { ViewAdminComponent } from './pages/view-admin/view-admin.component';
+import { ListaServiciosComponent } from './pages/management-services/lista-servicios/lista-servicios.component';
+import { CrearServicioComponent } from './pages/management-services/crear-servicios/crear-servicios.component';
+import { ActualizarTarifaComponent } from './pages/management-services/actualizar-tarifas/actualizar-tarifas.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'register', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
-<<<<<<< HEAD
-<<<<<<< HEAD
   { path: 'login', component: LoginComponent },
-  { path: 'auth', component: AuthComponent },
-
+  { path: 'view-client', component: ViewClientComponent },
+  { path: 'view-admin', component: ViewAdminComponent }, // Corregido el path
+  { path: 'servicios', component: ListaServiciosComponent },
+  { path: 'crear-servicio', component: CrearServicioComponent },
+  { path: 'tarifas', component: ActualizarTarifaComponent },
 ];
-
-=======
-  // Puedes agregar el login luego
-];
->>>>>>> 059de6d (chore: commit inicial)
-=======
-  // Puedes agregar el login luego
-];
-=======
-  { path: 'login', component: LoginComponent },
-  { path: 'auth', component: AuthComponent },
-
-];
-
->>>>>>> b213279 (feature/HU-04)
->>>>>>> c5bbe94 (feature/HU-04)
